e08f24bc6d92a0057acf0d42ab06a179b7691aa4
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-01-16 03:05:34 +0700

    add post model

---
+++ app/Models/Post.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+class Post
+{
+    private static $blog_posts = [
+        [
+            "title" => "How to Win Friends",
+            "slug" => "how-to-win-friends",
+            "author" => "Dale",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste mollitia, tempore natus nisi aut vel fugiat ab consequuntur dolore architecto illum exercitationem dicta, reiciendis sunt quidem cumque odio quam blanditiis?"
+        ],
+        [
+            "title" => "Alpha Girls",
+            "slug" => "alpha-girls",
+            "author" => "Henry",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam architecto, provident a atque voluptatem quos, facere soluta doloribus, dignissimos error dicta quasi. Odio excepturi quibusdam, cupiditate perspiciatis sequi eaque mollitia assumenda commodi autem. Iste dolorem veniam cumque suscipit tempora sint eius quae voluptates et deleniti velit voluptatibus iusto repellendus, quis dolor illum accusamus eveniet incidunt aliquam quasi quaerat cum dolorum natus ipsum. At similique temporibus velit dicta architecto voluptas saepe est cum et ea fugit reprehenderit quae ab id iure repellat, aliquam, quos delectus magnam animi quis? Aut voluptatem commodi assumenda molestiae quas libero ex maxime, recusandae dolorum iusto rem."
+        ],
+    ];
+
+    public static function all() {
+        return self::$blog_posts;
+    }
+
+    public static function find($slug) {
+        $posts = self::$blog_posts;
+        $post = [];
+
+        foreach($posts as $p) {
+            if($p["slug"] === $slug) {
+                $post = $p;
+            }
+        }
+
+        return $post;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,54 +29,16 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_post = [
-        [
-            "title" => "How to Win Friends",
-            "slug" => "how-to-win-friends",
-            "author" => "Dale",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste mollitia, tempore natus nisi aut vel fugiat ab consequuntur dolore architecto illum exercitationem dicta, reiciendis sunt quidem cumque odio quam blanditiis?"
-        ],
-        [
-            "title" => "Alpha Girls",
-            "slug" => "alpha-girls",
-            "author" => "Henry",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam architecto, provident a atque voluptatem quos, facere soluta doloribus, dignissimos error dicta quasi. Odio excepturi quibusdam, cupiditate perspiciatis sequi eaque mollitia assumenda commodi autem. Iste dolorem veniam cumque suscipit tempora sint eius quae voluptates et deleniti velit voluptatibus iusto repellendus, quis dolor illum accusamus eveniet incidunt aliquam quasi quaerat cum dolorum natus ipsum. At similique temporibus velit dicta architecto voluptas saepe est cum et ea fugit reprehenderit quae ab id iure repellat, aliquam, quos delectus magnam animi quis? Aut voluptatem commodi assumenda molestiae quas libero ex maxime, recusandae dolorum iusto rem."
-        ],
-    ];
-
+Route::get('/posts', function () {
     return view('posts', [
         "title" => "Posts",
-        "posts" => $blog_post
+        "posts" => Post::all()
     ]);
 });
 
-
 Route::get('/posts/{slug}', function($slug) {
-    $blog_post = [
-        [
-            "title" => "How to Win Friends",
-            "slug" => "how-to-win-friends",
-            "author" => "Dale",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste mollitia, tempore natus nisi aut vel fugiat ab consequuntur dolore architecto illum exercitationem dicta, reiciendis sunt quidem cumque odio quam blanditiis?"
-        ],
-        [
-            "title" => "Alpha Girls",
-            "slug" => "alpha-girls",
-            "author" => "Henry",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam architecto, provident a atque voluptatem quos, facere soluta doloribus, dignissimos error dicta quasi. Odio excepturi quibusdam, cupiditate perspiciatis sequi eaque mollitia assumenda commodi autem. Iste dolorem veniam cumque suscipit tempora sint eius quae voluptates et deleniti velit voluptatibus iusto repellendus, quis dolor illum accusamus eveniet incidunt aliquam quasi quaerat cum dolorum natus ipsum. At similique temporibus velit dicta architecto voluptas saepe est cum et ea fugit reprehenderit quae ab id iure repellat, aliquam, quos delectus magnam animi quis? Aut voluptatem commodi assumenda molestiae quas libero ex maxime, recusandae dolorum iusto rem."
-        ],
-    ];
-
-    $new_post = [];
-    foreach($blog_post as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
     return view('post', [
         "title" => "Single Post",
-        "post" => $new_post
+        "post" => Post::find($slug)
     ]);
 });
