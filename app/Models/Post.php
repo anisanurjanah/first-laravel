021646fa5094afc0a19dd65909efa44c11530e32
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-01-19 20:58:10 +0700

    add post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,30 +2,13 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "How to Win Friends",
-            "slug" => "how-to-win-friends",
-            "author" => "Dale",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste mollitia, tempore natus nisi aut vel fugiat ab consequuntur dolore architecto illum exercitationem dicta, reiciendis sunt quidem cumque odio quam blanditiis?"
-        ],
-        [
-            "title" => "Alpha Girls",
-            "slug" => "alpha-girls",
-            "author" => "Henry",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam architecto, provident a atque voluptatem quos, facere soluta doloribus, dignissimos error dicta quasi. Odio excepturi quibusdam, cupiditate perspiciatis sequi eaque mollitia assumenda commodi autem. Iste dolorem veniam cumque suscipit tempora sint eius quae voluptates et deleniti velit voluptatibus iusto repellendus, quis dolor illum accusamus eveniet incidunt aliquam quasi quaerat cum dolorum natus ipsum. At similique temporibus velit dicta architecto voluptas saepe est cum et ea fugit reprehenderit quae ab id iure repellat, aliquam, quos delectus magnam animi quis? Aut voluptatem commodi assumenda molestiae quas libero ex maxime, recusandae dolorum iusto rem."
-        ],
-    ];
+    use HasFactory;
 
-    public static function all() {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug) {
-        $posts = static::all();
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.main')
 
 @section('container')
-    <article class="mb-5">
-        <h2>{{ $post["title"] }}</h2>
-        <h5>By: {{ $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
-    </article>
+    <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <a href="/posts">Back to Posts</a>
+    {!! $post->body !!}
+
+    <a href="/posts">Back to posts</a>
 @endsection
