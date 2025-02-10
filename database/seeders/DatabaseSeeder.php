03e9e5776e0e664aa4ab0727a75d474f081c6e2e
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-02-10 09:53:57 +0700

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,62 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::create([
+            'name' => 'Anisa Nurjanah',
+            'email' => 'anisanurjanah2705@example.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        \App\Models\User::create([
+            'name' => 'Neilr',
+            'email' => 'neilr4419@example.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        \App\Models\Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming',
+        ]);
+
+        \App\Models\Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        \App\Models\Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad id aliquid provident assumenda',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad id aliquid provident assumenda, dolore, harum non dignissimos illum totam dolorem exercitationem quam cupiditate, reprehenderit culpa eum officiis maiores praesentium beatae. Soluta labore commodi, expedita error fuga tempora laboriosam quos harum voluptas dolor reiciendis molestias numquam enim aliquid non provident culpa vero quasi iusto consequuntur obcaecati magnam asperiores in. Omnis, ad tenetur. Nihil, nam placeat! Aut recusandae perspiciatis iure tempore consequuntur est repellendus vel, cumque delectus, laboriosam architecto.',
+            'category_id' => 1,
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad id aliquid provident assumenda',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad id aliquid provident assumenda, dolore, harum non dignissimos illum totam dolorem exercitationem quam cupiditate, reprehenderit culpa eum officiis maiores praesentium beatae. Soluta labore commodi, expedita error fuga tempora laboriosam quos harum voluptas dolor reiciendis molestias numquam enim aliquid non provident culpa vero quasi iusto consequuntur obcaecati magnam asperiores in. Omnis, ad tenetur. Nihil, nam placeat! Aut recusandae perspiciatis iure tempore consequuntur est repellendus vel, cumque delectus, laboriosam architecto.',
+            'category_id' => 1,
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad id aliquid provident assumenda',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad id aliquid provident assumenda, dolore, harum non dignissimos illum totam dolorem exercitationem quam cupiditate, reprehenderit culpa eum officiis maiores praesentium beatae. Soluta labore commodi, expedita error fuga tempora laboriosam quos harum voluptas dolor reiciendis molestias numquam enim aliquid non provident culpa vero quasi iusto consequuntur obcaecati magnam asperiores in. Omnis, ad tenetur. Nihil, nam placeat! Aut recusandae perspiciatis iure tempore consequuntur est repellendus vel, cumque delectus, laboriosam architecto.',
+            'category_id' => 2,
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad id aliquid provident assumenda',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad id aliquid provident assumenda, dolore, harum non dignissimos illum totam dolorem exercitationem quam cupiditate, reprehenderit culpa eum officiis maiores praesentium beatae. Soluta labore commodi, expedita error fuga tempora laboriosam quos harum voluptas dolor reiciendis molestias numquam enim aliquid non provident culpa vero quasi iusto consequuntur obcaecati magnam asperiores in. Omnis, ad tenetur. Nihil, nam placeat! Aut recusandae perspiciatis iure tempore consequuntur est repellendus vel, cumque delectus, laboriosam architecto.',
+            'category_id' => 2,
+            'user_id' => 2,
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,9 @@
 
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
-    <p>By: Anisa Nurjanah in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
+    <p>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
 
-    <a href="/posts">Back to posts</a>
+    <a href="/posts" class="d-block mt-4">Back to posts</a>
 @endsection
