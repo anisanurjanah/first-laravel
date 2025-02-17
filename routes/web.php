801a1e5a2c4770ba8235fb1164300291fc2c3b83
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-02-17 07:40:47 +0700

    fixed query looping n+1 problem

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::get('/categories/{category:slug}', [CategoryController::class, 'show']);
 
 Route::get('/authors/{author:username}', function(User $author) {
     return view('posts', [
-        "title" => "User Posts",
-        "posts" => $author->posts
+        "title" => "Post By Author : $author->name",
+        "posts" => $author->posts->load('category', 'author')
     ]);
 });
