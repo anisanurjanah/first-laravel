cd80af0aef83585d2eb94ec47cdcbd4b0fecee39
Anisa Nurjanah <anisanurjanah2705@gmail.com>
2025-03-01 04:17:29 +0700

    fixed sidebar

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,38 +1,34 @@
-<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary d-flex flex-column vh-100">
+    <div class="offcanvas-md offcanvas-end bg-body-tertiary d-flex flex-column flex-grow-1" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
         </div>
-        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto flex-grow-1">
             <ul class="nav flex-column">
                 <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard">
-                        <i class="bi bi-house"></i>
+                        <svg class="bi text-primary" fill="currentColor"><use xlink:href="#house-fill"/></svg>
                         Dashboard
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/posts*') ? 'active' : '' }}" href="/dashboard/posts">
-                        <i class="bi bi-file-earmark-text"></i>
+                        <svg class="bi text-primary" fill="currentColor"><use xlink:href="#file-earmark-text"/></svg>
                         My Post
                     </a>
                 </li>
             </ul>
 
             @can('admin')
-                <hr class="my-3">
-
-                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
                     <span>Administrator</span>
                 </h6>
-
-                <hr class="my-3">
 
                 <ul class="nav flex-column">
                     <li class="nav-item">
                         <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
-                            <i class="bi bi-grid"></i>
+                            <svg class="bi text-primary" fill="currentColor"><use xlink:href="#file-earmark-text"/></svg>
                             Post Categories
                         </a>
                     </li>
@@ -46,7 +42,7 @@
                     <form action="/logout" method="post">
                         @csrf
                         <button type="submit" class="nav-link d-flex align-items-center gap-2">
-                            <i class="bi bi-box-arrow-right"></i>
+                            <svg class="bi text-primary" fill="currentColor"><use xlink:href="#door-closed"/></svg>
                             Logout
                         </button>
                     </form>
